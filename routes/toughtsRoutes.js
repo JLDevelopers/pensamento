52ccc6d829a04de89517fef104831be043f4ad85
JLDevelopers <163607601+JLDevelopers@users.noreply.github.com>
2024-06-10 15:29:01 -0300

    Add files via upload

---
+++ routes/toughtsRoutes.js
@@ -0,0 +1,15 @@
+// toughtsRoutes.js
+const express = require("express");
+const router = express.Router();
+const ToughtController = require("../controllers/ToughtController");
+const checkAuth = require("../helpers/auth").checkAuth;
+
+router.get("/add", checkAuth, ToughtController.createTought);
+router.post("/add", checkAuth, ToughtController.createToughtSave);
+router.post("/remove", checkAuth, ToughtController.removeTought);
+router.get("/edit/:id", checkAuth, ToughtController.updateTought);
+router.post("/edit", checkAuth, ToughtController.updateToughtPost);
+router.get("/dashboard", checkAuth, ToughtController.dashboard); // Middleware aplicado aqui
+router.get("/", ToughtController.showToughts);
+
+module.exports = router;
